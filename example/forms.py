2d161d6c4378e34c67d24a686ced9046d11c43e1
Sachit Khadka <sachitkhadka73@gmail.com>
2024-01-17 17:01:11 +0545

    improv labels

--- example/forms.py
+++ example/forms.py
@@ -5,3 +5,7 @@ class ResumeForm(forms.ModelForm):
     class Meta:
         model=ResumeModel
         fields="__all__"
+        labels={'name':'Full Name','dob':'Date of Birth','gender':'Gender',
+                'locality':'Locality','city':'City','pin':'Postal Code',
+                'state':'State','mobile':'Mobile','email':'Email','job_city':'Job Location',
+                'image':'Profile Picture','Upload your Resume':'file'}
