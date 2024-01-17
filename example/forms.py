ab844ebc50d6698d87e4e6c51835ce3d825b9ceb
Sachit Khadka <sachitkhadka73@gmail.com>
2024-01-17 17:13:33 +0545

    bootstrap and sty

--- example/forms.py
+++ example/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import ResumeModel
 
+Gender_Choices=(
+    ('Male','Male'),
+    ('Female','Female'),
+    ('Others','Others')
+)
+
 class ResumeForm(forms.ModelForm):
+    gender=forms.ChoiceField(choices=Gender_Choices,widget=forms.RadioSelect)
     class Meta:
         model=ResumeModel
         fields="__all__"
@@ -9,3 +16,15 @@ class ResumeForm(forms.ModelForm):
                 'locality':'Locality','city':'City','pin':'Postal Code',
                 'state':'State','mobile':'Mobile','email':'Email','job_city':'Job Location',
                 'image':'Profile Picture','Upload your Resume':'file'}
+        widgets={
+            'name':forms.TextInput(attrs={'class':'form-control'}),
+            'dob':forms.TextInput(attrs={'class':'form-control'}),
+            'gender':forms.TextInput(attrs={'class':'form-control'}),
+            'locality':forms.TextInput(attrs={'class':'form-control'}),
+            'city':forms.TextInput(attrs={'class':'form-control'}),
+            'pin':forms.TextInput(attrs={'class':'form-control'}),
+            'state':forms.TextInput(attrs={'class':'form-control'}),
+            'mobile':forms.TextInput(attrs={'class':'form-control'}),
+            'email':forms.TextInput(attrs={'class':'form-control'}),
+            'job_city':forms.TextInput(attrs={'class':'form-control'}),
+            }
