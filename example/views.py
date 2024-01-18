6f446955014b7dc6dd9c546f2c9c188c8c6e2e52
Sachit Khadka <sachitkhadka73@gmail.com>
2024-01-18 09:58:43 +0545

    dis candi nd re error fixes

--- example/views.py
+++ example/views.py
@@ -1,14 +1,17 @@
 from .forms import ResumeForm
 from django.views import View
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import ResumeModel
 
 class Home(View):
     def get(self,request):
         form=ResumeForm()
+        candidates=ResumeModel.objects.all()
 
         return render(request,'example/home.html',{
-            'form':form
+            'form':form,
+            'candidates':candidates
         })
     def post(self,request):
         if request.POST:
@@ -17,6 +20,7 @@ class Home(View):
             if form.is_valid():
                 form.save()
                 print('Sucess!')
+            return redirect('/')  
         return render(request,'example/home.html',{
             'form':form
         })        
